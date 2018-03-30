adf703b94577cab26e4761a057efad565176d521
girishkumarshakya <admin@androidacademia.com>
2018-03-30 23:11:49 +0530

    update manifest with pip mode

--- app/src/main/java/pipmode/androidacademia/com/pictureinpicturedemo/MainActivity.java
+++ app/src/main/java/pipmode/androidacademia/com/pictureinpicturedemo/MainActivity.java
@@ -1,13 +1,28 @@
 package pipmode.androidacademia.com.pictureinpicturedemo;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+    private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = (VideoView)findViewById(R.id.videoView);
+        videoView.setOnLongClickListener(this);
     }
+
+    @Override
+    public boolean onLongClick(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            enterPictureInPictureMode();
+        }
+        return false;
+    }
+
+
 }
