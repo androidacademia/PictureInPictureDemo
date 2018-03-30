712c4aad3917af2e3c2b35c7252de3f10c427beb
girishkumarshakya <admin@androidacademia.com>
2018-03-30 23:16:18 +0530

    update manifest with pip mode

--- app/src/main/java/pipmode/androidacademia/com/pictureinpicturedemo/MainActivity.java
+++ app/src/main/java/pipmode/androidacademia/com/pictureinpicturedemo/MainActivity.java
@@ -1,5 +1,6 @@
 package pipmode.androidacademia.com.pictureinpicturedemo;
 
+import android.content.res.Configuration;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
     private VideoView videoView;
+    android.support.v7.app.ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +27,13 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         return false;
     }
 
-
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+        if (isInPictureInPictureMode){
+            actionBar.hide();
+        }else {
+            actionBar.show();
+        }
+    }
 }
